3c4947350fd02e75efe5ac0d0be083c3b7895b2e
nitamuskan <nita.muskan.8810@gmail.com>
2024-08-14 22:31:36 +0530

    first commit

---
+++ lib/utils.ts
@@ -0,0 +1,9 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+export function handleError(error: any) {
+  console.error('Error:', error);
+}
